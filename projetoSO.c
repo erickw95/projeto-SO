4be3bc180a3c37bd64ebe4854bd2daab4998ba40
erickw95 <erickw95@gmail.com>
2017-06-15 11:52:47 -0300

    Update projetoSO.c

--- projetoSO.c
+++ projetoSO.c
@@ -1,32 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <semaphore.h>
 
-#define NUM_THREADS 6
-//#define SEM 1
-int SEM = 1;
-int ct = 0;
+#define N_THREAD 2
+sem_t teste;
 
 void *funcao(void *arg)
 {
-   printf("func 1\n");
-   return (NULL);
+	printf("t1 %d\n", teste);// entendendo wait
+
+	sem_trywait(&teste);
+
+	printf("t2 %d\n", teste);// entendendo wait
+
+	int *pvalor;
+	pvalor = arg;
+	printf("Thread %d executando... \n", *pvalor);
+	printf("Thread %d finalizada! \n", *pvalor);
+
+	sem_post(&teste);
 }
 
-int down(SEM S)// Wait
+int main()
 {
-  if(S == 0)
-  {
-    //bloqueia processo;
-    S--;
-  }
-  return SEM;
-}
+	sem_init(&teste, 0, 0);
+	pthread_t threads[N_THREAD];
+	int i, arg[N_THREAD];
 
-int up(SEM S)// Signal
-{
-  if(S != 0)
-    S++;
-  //else
-    //desbloqueia processo;
-  return SEM;
+	for(i = 0; i < N_THREAD; i++){
+		arg[i] = i + 1;
+		pthread_create(&threads[i], NULL, funcao, (void *)&arg[i]);
+	}
+
+//	for(i = 0; i < N_THREAD; i++)
+//		pthread_join(threads[i], NULL);
+
+	printf("\n");
+
+	return 0;
+}
