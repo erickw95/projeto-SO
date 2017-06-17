2f03d15d146c2a87bc1ed9ff4c76e958e3ec377d
erickw95 <erickw95@gmail.com>
2017-06-17 18:30:36 -0300

    Criação de um buffer e retirndo o uso de for

--- projetoSO.c
+++ projetoSO.c
@@ -1,42 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <pthread.h>
 #include <semaphore.h>
 
-#define N_THREAD 2
-sem_t teste;
+void myfunc1 ( void *ptr );
+void myfunc2 ( void *ptr );
 
-void *funcao(void *arg)
+char buf[20];// Variavel global de 20 Byts
+sem_t mutex;
+
+int main(int argc, char *argv[])
 {
-	printf("t1 %d\n", teste);// entendendo wait
+  pthread_t thread1;
+  pthread_t thread2;
 
-	sem_trywait(&teste);
+  char *msg1 = "Thread 1";
+  char *msg2 = "Thread 2";
 
-	printf("t2 %d\n", teste);// entendendo wait
+  /* Parametros sem_init
+   o primeiro é o semaforo
+   o segundo 1 ou 0 representa true or false, se pode ou nao usar em outro processo
+   o terceiro é o valor inicial do semaforo */
+  sem_init(&mutex, 0, 1);
 
-	int *pvalor;
-	pvalor = arg;
-	printf("Thread %d executando... \n", *pvalor);
-	printf("Thread %d finalizada! \n", *pvalor);
+  // Cada thread usa uma funcao
+  pthread_create (&thread1, NULL, (void *) &myfunc1, (void *) msg1);
+  pthread_create (&thread2, NULL, (void *) &myfunc2, (void *) msg2);
 
-	sem_post(&teste);
+  pthread_join(thread1, NULL);
+  pthread_join(thread2, NULL);
+
+  sem_destroy(&mutex);
+
+  return 0;
 }
 
-int main()
+// Nas funcoes usamos sem_wait e sem_post para proteger o dado, para que não leia antes de escrever
+void myfunc1 ( void *ptr)// Essa thread escreve
 {
-	sem_init(&teste, 0, 0);
-	pthread_t threads[N_THREAD];
-	int i, arg[N_THREAD];
+  char *msg = (char *)ptr;
 
-	for(i = 0; i < N_THREAD; i++){
-		arg[i] = i + 1;
-		pthread_create(&threads[i], NULL, funcao, (void *)&arg[i]);
-	}
+  printf("%s\n", msg);
 
-//	for(i = 0; i < N_THREAD; i++)
-//		pthread_join(threads[i], NULL);
+  sem_wait(&mutex);// Trava a thread
+  sprintf(buf, "%s", "Estou no buf");
+  sleep(1);
+  sem_post(&mutex);// Libera a thread
 
-	printf("\n");
+  pthread_exit(0);
+}
 
-	return 0;
+void myfunc2 (void *ptr)// Essa thread le
+{
+  char *msg = (char *)ptr;
+
+  printf("%s\n", msg);
+
+  sem_wait(&mutex);
+  printf("%s\n", buf);
+  sleep(1);
+  sem_post(&mutex);
+
+  pthread_exit(0);
 }
